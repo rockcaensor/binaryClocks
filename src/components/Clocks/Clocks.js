567ed8554782fb2975ba6efbc8df6b3c1be83c27
rockcaensor <46659725+rockcaensor@users.noreply.github.com>
2020-07-24 13:47:09 +0300

    binary display

--- src/components/Clocks/Clocks.js
+++ src/components/Clocks/Clocks.js
@@ -9,14 +9,37 @@ class Clocks extends React.Component {
     clearInterval(this.timer);
   }
 
+  stringToNumber(string) {
+    let a = [];
+    for (let i = 0; i < string.length; i++) {
+      a.push(Number(string[i]));
+      console.log(typeof a[i]);
+    }
+
+    for (let i = 0; i < 6 - string.length; i++) {
+      a.unshift(0);
+    }
+
+    return a;
+  }
+
   render() {
     return (
       <div>
         <span>
-          {this.props.ClocksPage.time.getHours()} :{" "}
-          {this.props.ClocksPage.time.getMinutes()} :{" "}
-          {this.props.ClocksPage.time.getSeconds()}
+          {this.stringToNumber(
+            this.props.ClocksPage.time.getHours().toString(2)
+          )}{" "}
+          :{" "}
+          {this.stringToNumber(
+            this.props.ClocksPage.time.getMinutes().toString(2)
+          )}{" "}
+          :{" "}
+          {this.stringToNumber(
+            this.props.ClocksPage.time.getSeconds().toString(2)
+          )}
         </span>
+        <div>{1 + 2 ? <div>{1 + 2}</div> : <div>0</div>}</div>
       </div>
     );
   }
